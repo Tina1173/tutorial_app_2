cf06ce5b62b0f5da1426929ba6213a1a1ef1b98a
Tina1173 <chinatsuphonics@gmail.com>
2019-05-09 02:43:24 +0000

    basic login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,28 +1,28 @@
 class UsersController < ApplicationController
 
-  def show
+def show
    @user = User.find(params[:id])
-  end
+end
 
-  def new
-   @user = User.new # 新規作成されたUserオブジェクトをインスタンス変数に代入します
-  end
+def new
+   @user = User.new 
+end
 
   def create
     @user = User.new(user_params)
     if @user.save
-     flash[:success] = "ユーザーの新規作成に成功しました。"
-     redirect_to @user
+      log_in @user
+      flash[:success] = "ユーザーの新規作成に成功しました。"
+      redirect_to @user
     else
- 
-     render 'new'
-
+      render 'new'
     end
   end
 
-private
+  private
 
-  def user_params
-   params.require(:user).permit(:name, :email, :password, :password_confirmation)
-  end
+    def user_params
+      params.require(:user).permit(:name, :email, :password,
+                                   :password_confirmation)
+    end
 end
